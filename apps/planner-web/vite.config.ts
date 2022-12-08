9a61d33d99eea0484f59247200afa8c894ad6f76
viktormarinho <viktormpcs@gmail.com>
2022-12-08 14:42:15 -0300

    fix(dev server): fixed frontend server ports

--- apps/planner-web/vite.config.ts
+++ apps/planner-web/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001
+    port: 3001,
+    strictPort: true
   },
   plugins: [react()]
 })
