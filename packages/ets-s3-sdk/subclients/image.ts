f2a0394a874323f6d11415e93363624881611f23
viktormarinho <viktormpcs@gmail.com>
2022-12-26 23:38:57 -0300

    feat(ets-s3-sdk): created S3 SDK base. Simple image upload working

---
+++ packages/ets-s3-sdk/subclients/image.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosError } from "axios";
+
+type FileFromS3 = {
+    id: string
+    type: string
+    path: string
+    meta?: string
+    createdAt: Date
+}
+
+type UploadImageParams = {
+  file: File;
+  quality?: number;
+  meta?: any;
+};
+
+type UploadImageOk = {
+    ok: true
+    file: FileFromS3
+}
+
+type UploadImageErr = {
+    ok: false
+    err: AxiosError
+}
+
+export class ImageClient {
+  private uploadUrl: string;
+
+  constructor(url: string) {
+    this.uploadUrl = `${url}/upload/image`;
+  }
+
+  private getFormData(object: any) {
+    const formData = new FormData();
+    Object.keys(object).forEach((key) => formData.append(key, object[key]));
+    return formData;
+  }
+
+  async upload(opts: UploadImageParams, headers?: any): Promise<UploadImageOk | UploadImageErr> {
+    const data = this.getFormData(opts);
+
+    try {
+        const res = await axios.post(this.uploadUrl, data, {
+          headers: { "Content-Type": "multipart/form-data", ...headers },
+        });
+
+        return { ok: true, file: res.data.file }
+    } catch (err: unknown) {
+        return { ok: false, err } as UploadImageErr
+    }
+  }
+}
