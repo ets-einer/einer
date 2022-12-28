6fd15e7c449e8586b2a5d702af29ce90bd074094
abehidek <hidek.abe@outlook.com>
2022-12-27 21:52:20 -0300

    test(mock-s3-sdk): setup env vars for mock

--- tests/mock-s3-sdk/src/App.tsx
+++ tests/mock-s3-sdk/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-import { S3Client } from 'ets-s3-sdk';
+import { useState } from "react";
+import "./App.css";
+import { S3Client } from "ets-s3-sdk";
 
-const s3 = new S3Client({ url: "http://localhost:5001" });
-
+const s3 = new S3Client({
+  url: import.meta.env.VITE_SERVICE_S3_URL || "http://localhost:5001",
+});
 
 function App() {
   const [file, setFile] = useState<File>();
@@ -13,25 +14,22 @@ function App() {
       const res = await s3.image.upload({ file });
 
       if (res.ok) {
-        alert('UPLOADED AT ' +  res.file.path);
+        alert("UPLOADED AT " + res.file.path);
       } else {
-        alert('ERROR ' + res.err)
+        alert("ERROR " + res.err);
       }
     }
-  }
+  };
 
   return (
     <div className="App">
-      <input type="file" onChange={e => setFile(e.target.files![0])} />
-
+      <input type="file" onChange={(e) => setFile(e.target.files![0])} />
       <br />
-
       {file?.name} selected.
-
       <br />
       <button onClick={upload}>Upload</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tests/mock-s3-sdk/vite.config.ts
+++ tests/mock-s3-sdk/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const config = ({ mode }) => {
+  process.env = Object.assign(
+    process.env,
+    loadEnv(mode, path.normalize(process.cwd() + "/../../"), "")
+  );
+
+  return defineConfig({
+    server: {
+      //@ts-ignore
+      port: process.env.VITE_TEST_S3_SDK_PORT || 5173,
+      strictPort: true,
+    },
+    plugins: [react()],
+  });
+};
+
+export default config;
