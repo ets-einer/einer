513c81679aa92958bda20eee0f7ce9020192763b
abehidek <hidek.abe@outlook.com>
2022-12-29 20:57:48 -0300

    feat(server-template): add findMany example to protectedHello

--- apps/server-template/src/routers/example.ts
+++ apps/server-template/src/routers/example.ts
@@ -2,16 +2,18 @@ import { publicProcedure, router, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
 export const exampleRouter = router({
-  hello: publicProcedure.query(({ ctx, input }) => {
+  hello: publicProcedure.query(({}) => {
     return { msg: "Hello!" };
   }),
   protectedHello: protectedProcedure
     .input(z.object({ number: z.number() }))
-    .query(({ ctx, input }) => {
+    .query(async ({ ctx, input }) => {
+      const exampleData = await ctx.prisma.example.findMany()
       return {
         msg: "Protected Hello!",
         number: input.number + 2,
         userEmail: ctx.user.email,
+        exampleData
       };
     }),
 });
