f1133d761e066d787e2d139427b18cdf0e5da285
abehidek <hidek.abe@outlook.com>
2022-12-29 23:34:03 -0300

    feat(auth/prisma): add create permissions and users seed

---
+++ apps/auth/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { hashPassword } from "@src/lib/hash";
+import { prisma } from "@src/lib/prisma";
+
+async function runSeed() {
+  await prisma.user.deleteMany();
+  await prisma.permission.deleteMany();
+
+  const permissionUser = await prisma.permission.create({
+    data: {
+      id: 0,
+      name: "USER",
+    },
+  });
+
+  const permissionAdmin = await prisma.permission.create({
+    data: {
+      id: 1,
+      name: "ADMIN",
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      email: "zamudo@gmail.com",
+      passwordHash: await hashPassword("123"),
+      permissions: {
+        connect: {
+          id: permissionUser.id,
+        },
+      },
+    },
+  });
+
+  const userCelso = await prisma.user.create({
+    data: {
+      email: "celso@gmail.com",
+      passwordHash: await hashPassword("celso"),
+      permissions: {
+        connect: {
+          id: permissionUser.id,
+        },
+      },
+    },
+  });
+
+  await prisma.user.update({
+    where: {
+      id: userCelso.id,
+    },
+    data: {
+      permissions: {
+        connect: {
+          id: permissionAdmin.id,
+        },
+      },
+    },
+  });
+}
+
+runSeed();
