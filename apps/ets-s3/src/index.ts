cf4b4c9877f0b6fca184458965063677bc197633
viktormarinho <viktormpcs@gmail.com>
2023-01-03 22:55:57 -0300

    test & fix(ets-s3): add adminrouter to index and update insomnia tests

--- apps/ets-s3/src/index.ts
+++ apps/ets-s3/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import imageRouter from "./routers/image";
 import fileRouter from "./routers/file";
+import adminRouter from "./routers/admin";
 
 const S3_PORT = process.env.SERVICE_S3_PORT || 5001;
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static("./public"));
 app.use(imageRouter.router);
 app.use(fileRouter.router);
+app.use(adminRouter.router);
 
 app.listen(S3_PORT, () => {
   logger.log(`Server listening on port ${S3_PORT}`);
